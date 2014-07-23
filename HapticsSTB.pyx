7e616d51b222e910590db00d8aa35f0ebdf82b81
Conor O'Brien <conoreobrien@gmail.com>
2014-07-23 18:42:47 -0400

    Code Cleanup
    Graphing functions moved to HapticsDAQ.pyx, comments are clarified
    throughout

--- HapticsSTB.pyx
+++ HapticsSTB.pyx
@@ -2,6 +2,7 @@
 
 import numpy as np
 cimport numpy as np
+import pylab as pl
 import pdb
 
 # Calibrated linear transform for out Mini40, from channel voltages to forces
@@ -54,6 +55,12 @@ def Serial2Acc(str x):
 
 # [C0, C1, C2, C3, C4, C5] = Serial2M40Volts(data)
 
+def Serial2Data(str x, np.ndarray[np.float64_t, ndim = 1] bias):
+	FT = Serial2FT(x, bias)
+	ACC = Serial2Acc(x)
+
+	return np.hstack((FT, ACC))
+
 def Serial2M40Volts(str x):
 	cdef int i, j, y
 	volts = np.zeros((6), dtype = np.float64)
@@ -67,4 +74,149 @@ def Serial2M40Volts(str x):
 
 	return volts
 
+def GraphingSetup(inputs):
 
+	line_length = inputs['line_length']
+	pl.ion()
+
+	# Force/Torque Graphing
+	if inputs['graphing'] == 1:
+
+		f, (axF, axT) =pl.subplots(2,1, sharex=True)
+
+		axF.axis([0,line_length,-20,20])
+		axF.grid()
+		axT.axis([0,line_length,-2,2])
+		axT.grid()
+
+
+		FXline, = axF.plot([0] * line_length, color = 'r')
+		FYline, = axF.plot([0] * line_length, color = 'g')
+		FZline, = axF.plot([0] * line_length, color = 'b')
+		TXline, = axT.plot([0] * line_length, color = 'c')
+		TYline, = axT.plot([0] * line_length, color = 'm')
+		TZline, = axT.plot([0] * line_length, color = 'y')
+
+		axF.legend([FXline, FYline, FZline], ['FX', 'FY', 'FZ'])
+		axT.legend([TXline, TYline, TZline], ['TX', 'TY', 'TZ'])
+
+		plot_objects = (FXline, FYline, FZline, TXline, TYline, TZline)
+
+		pl.draw()
+
+	# Mini40 Voltage Graphing
+	elif inputs['graphing'] == 2:
+		pl.axis([0,line_length,-5,5])
+		pl.grid()
+
+		C0line, = pl.plot([0] * line_length, color = 'brown')
+		C1line, = pl.plot([0] * line_length, color = 'yellow')
+		C2line, = pl.plot([0] * line_length, color = 'green')
+		C3line, = pl.plot([0] * line_length, color = 'blue')
+		C4line, = pl.plot([0] * line_length, color = 'purple')
+		C5line, = pl.plot([0] * line_length, color = 'gray')
+
+		pl.legend([C0line, C1line, C2line, C3line, C4line, C5line], 
+			['Channel 0', 'Channel 1','Channel 2','Channel 3','Channel 4','Channel 5'], loc=2)
+
+		plot_objects = (C0line, C1line, C2line, C3line, C4line, C5line)
+		pl.draw()
+
+	#Accelerometer Voltage Graphing
+	elif inputs['graphing'] == 3:
+
+		f, (ax1, ax2, ax3) =pl.subplots(3,1, sharex=True)
+
+		ax1.axis([0,line_length,0,3.3])
+		ax1.grid()
+		ax2.axis([0,line_length,0,3.3])
+		ax2.grid()
+		ax3.axis([0,line_length,0,3.3])
+		ax3.grid()
+
+		A1Xline, = ax1.plot([0] * line_length, color = 'r')
+		A1Yline, = ax1.plot([0] * line_length, color = 'g')
+		A1Zline, = ax1.plot([0] * line_length, color = 'b')
+
+		A2Xline, = ax2.plot([0] * line_length, color = 'r')
+		A2Yline, = ax2.plot([0] * line_length, color = 'g')
+		A2Zline, = ax2.plot([0] * line_length, color = 'b')
+
+		A3Xline, = ax3.plot([0] * line_length, color = 'r')
+		A3Yline, = ax3.plot([0] * line_length, color = 'g')
+		A3Zline, = ax3.plot([0] * line_length, color = 'b')
+
+		plot_objects = (A1Xline, A1Yline, A1Zline, A2Xline, A2Yline, A2Zline, A3Xline, A3Yline, A3Zline)
+		pl.draw()
+
+	# 2D Position Plotting
+	elif inputs['graphing'] == 4:
+
+		pl.axis([-.075, .075, -.075, .075])
+		pl.grid()
+		touch_point, = pl.plot(0,0, marker="o", markersize=50)
+
+		plot_objects = (touch_point,)
+		pl.draw()
+
+	else:
+		print "INVALID GRAPHING MODE"
+
+	return plot_objects
+
+def GraphingUpdater(inputs, np.ndarray[np.float64_t, ndim  = 2] data, plot_objects):
+
+	if inputs['graphing'] == 1:
+
+		plot_objects[0].set_ydata(data[:,0].T)
+		plot_objects[1].set_ydata(data[:,1].T)
+		plot_objects[2].set_ydata(data[:,2].T)
+		plot_objects[3].set_ydata(data[:,3].T)
+		plot_objects[4].set_ydata(data[:,4].T)
+		plot_objects[5].set_ydata(data[:,5].T)
+
+	if inputs['graphing'] == 2:
+
+		plot_objects[0].set_ydata(data[:,15].T)
+		plot_objects[1].set_ydata(data[:,16].T)
+		plot_objects[2].set_ydata(data[:,17].T)
+		plot_objects[3].set_ydata(data[:,18].T)
+		plot_objects[4].set_ydata(data[:,19].T)
+		plot_objects[5].set_ydata(data[:,20].T)
+	
+	if inputs['graphing'] == 3:
+
+		plot_objects[0].set_ydata(data[:,6].T)
+		plot_objects[1].set_ydata(data[:,7].T)
+		plot_objects[2].set_ydata(data[:,8].T)		
+
+		plot_objects[3].set_ydata(data[:,9].T)
+		plot_objects[4].set_ydata(data[:,10].T)
+		plot_objects[5].set_ydata(data[:,11].T)		
+
+		plot_objects[6].set_ydata(data[:,12].T)
+		plot_objects[7].set_ydata(data[:,13].T)
+		plot_objects[8].set_ydata(data[:,14].T)
+
+
+	if inputs['graphing'] == 4:
+
+		if abs(data[-1,2]) > .15:
+			x = -1*data[-1,4]/data[-1,2]
+			y = data[-1,3]/data[-1,2]
+		else:
+			x = y = 0
+
+		plot_objects[0].set_ydata(y)
+		plot_objects[0].set_xdata(x)
+
+	pl.draw()
+
+def to16bit(x):
+	if x > int('0b1111111111111111',2):
+		raise ValueError
+
+	high = (x&int('0b1111111100000000',2))>>8
+	low = x&int('0b11111111',2)
+
+	return chr(high)+chr(low)
--- ReadSerial.py
+++ ReadSerial.py
@@ -6,8 +6,7 @@ import numpy as np
 import serial, sys, os, glob, pdb, time
 from HapticsSTB import *
 
-
-# Default values for command line inputs, checked right below here
+# Dict for command line inputs, contains default values
 inputs = {	'graphing' : 0,
 			'line_length': 150,
 			'bias_sample': 100,
@@ -17,24 +16,25 @@ inputs = {	'graphing' : 0,
 			'sample_rate': 500,
 }
 
+# Message displayed for command line help
 help_message = """ ******
 -bias_sample: Number of samples averaged to get Mini40 biasing vector
--sample_time: Sampling time in seconds
+-sample_time: Length of sample in seconds
 -sample_rate: data sampling rate in Hz (forced to 500Hz for plotting)
 -write_data: Write data to timestamped file
--graphing: reduce sample rate and display line plots
+-graphing: reduce sample rate and display line plots for debugging
     1: F/T graph
     2: Mini40 Channel Voltages
     3: Accelerometer Voltages
     4: Single Point Position
 (GRAPHING OPTIONS)
--line_length: Length of graphing plot
+-line_length: number of samples shown on graph
 -update_interval: Number of samples between plot updates
 *****
 """
 
 # Input handling, inputs beginning with '-' are considered commands, following
-# string is converted to an int and assigned to the dictionary
+# string is converted to an int and assigned to the inputs dict
 try:
 	if len(sys.argv) > 1:
 		for arg in range(1,len(sys.argv)):
@@ -53,6 +53,14 @@ except (NameError, ValueError, IndexError):
 	print "Invalid Command!"
 	sys.exit()
 
+
+# Graphing Initialization
+if inputs['graphing']:
+
+	inputs['sample_rate'] = 750;
+	print 'Forcing sample rate to 500Hz for graphing'
+
+	plot_objects = GraphingSetup(inputs)
 
 # Open serial port, if default device not found list alternatives and ask for
 # input
@@ -82,108 +90,11 @@ except OSError:
 		sys.exit()
 
 
-# Graphing Initialization code
-if inputs['graphing']:
-
-	inputs['sample_rate'] = 500;
-	print 'Forcing sample rate to 500Hz for graphing'
-	line_length = inputs['line_length']
-	pl.ion()
-
-	# Force/Torque Graphing
-	if inputs['graphing'] == 1:
-
-		f, (axF, axT) =pl.subplots(2,1, sharex=True)
-
-		axF.axis([0,line_length,-20,20])
-		axF.grid()
-		axT.axis([0,line_length,-2,2])
-		axT.grid()
-
-
-		FXline, = axF.plot([0] * line_length, color = 'r')
-		FYline, = axF.plot([0] * line_length, color = 'g')
-		FZline, = axF.plot([0] * line_length, color = 'b')
-		TXline, = axT.plot([0] * line_length, color = 'c')
-		TYline, = axT.plot([0] * line_length, color = 'm')
-		TZline, = axT.plot([0] * line_length, color = 'y')
-
-		axF.legend([FXline, FYline, FZline], ['FX', 'FY', 'FZ'])
-		axT.legend([TXline, TYline, TZline], ['TX', 'TY', 'TZ'])
-
-
-
-		pl.draw()
-
-	# Mini40 Voltage Graphing
-	elif inputs['graphing'] == 2:
-		pl.axis([0,line_length,-5,5])
-		pl.grid()
-
-		C0line, = pl.plot([0] * line_length, color = 'brown')
-		C1line, = pl.plot([0] * line_length, color = 'yellow')
-		C2line, = pl.plot([0] * line_length, color = 'green')
-		C3line, = pl.plot([0] * line_length, color = 'blue')
-		C4line, = pl.plot([0] * line_length, color = 'purple')
-		C5line, = pl.plot([0] * line_length, color = 'gray')
-
-		pl.legend([C0line, C1line, C2line, C3line, C4line, C5line], 
-			['Channel 0', 'Channel 1','Channel 2','Channel 3','Channel 4','Channel 5'])
-
-		pl.draw()
-
-	#Accelerometer Voltage Graphing
-	elif inputs['graphing'] == 3:
-
-		f, (ax1, ax2, ax3) =pl.subplots(3,1, sharex=True)
-
-		ax1.axis([0,line_length,0,3.3])
-		ax1.grid()
-		ax2.axis([0,line_length,0,3.3])
-		ax2.grid()
-		ax3.axis([0,line_length,0,3.3])
-		ax3.grid()
-
-		A1Xline, = ax1.plot([0] * line_length, color = 'r')
-		A1Yline, = ax1.plot([0] * line_length, color = 'g')
-		A1Zline, = ax1.plot([0] * line_length, color = 'b')
-
-		A2Xline, = ax2.plot([0] * line_length, color = 'r')
-		A2Yline, = ax2.plot([0] * line_length, color = 'g')
-		A2Zline, = ax2.plot([0] * line_length, color = 'b')
-
-		A3Xline, = ax3.plot([0] * line_length, color = 'r')
-		A3Yline, = ax3.plot([0] * line_length, color = 'g')
-		A3Zline, = ax3.plot([0] * line_length, color = 'b')
-
-		pl.draw()
-
-	# 2D Position Plotting
-	elif inputs['graphing'] == 4:
-
-		pl.axis([-.075, .075, -.075, .075])
-		pl.grid()
-		touch_point, = pl.plot(0,0, marker="o", markersize=50)
-
-		pl.draw()
-
-	else:
-		print "INVALID GRAPHING MODE"
-
-
 
 # Try to read from serial port, if you don't get anything close and retry up
 # to five times
 
-def to16bit(x):
-	if x > int('0b1111111111111111',2):
-		raise ValueError
-
-	high = (x&int('0b1111111100000000',2))>>8
-	low = x&int('0b11111111',2)
-
-	return chr(high)+chr(low)
-
+# Send a stop/start command in case of improper stop last time
 ser.flush()
 ser.write('\x02')
 ser.write('\x01' + to16bit(inputs['sample_rate']))
@@ -197,7 +108,7 @@ for ii in range(1,6):
 
 		print 'Packet empty, retry #%d' %ii
 		ser.close()
-		
+
 		if alt_device:
 			ser = serial.Serial(alt_device,6900, timeout=0.1)
 		else:
@@ -250,16 +161,18 @@ print bias
 
 ## SAMPLING
 # Code takes samples for seconds defined in sample_time
-# need to preallocate vectors
 
 print 'STARTING DATA COLLECTION'
 start = time.time()
 num_samples = inputs['sample_rate']*inputs['sample_time']
 
-FT_hist = np.zeros((num_samples, 6))
+# If plotting voltage as well, DAT_hist has an extra six columns at the end
+# which contain the voltage channels
+
 if inputs['graphing'] == 2:
-	V_hist = np.zeros((num_samples,6))
-ACC_hist = np.zeros((num_samples, 9))
+	DAT_hist = np.zeros((num_samples,21))
+else:
+	DAT_hist = np.zeros((num_samples, 15))
 
 for ii in range(0,num_samples):
 
@@ -270,83 +183,35 @@ for ii in range(0,num_samples):
 		ser.close()
 		sys.exit()
 
+	# Missed packet detection, last byte of usb packet counts up
 	packet = ord(dat[30])
-	
 	if ii == 0:
 		packet_old = packet
-	elif packet > (packet_old+1)%256:
+	elif packet != (packet_old+1)%256:
 		print 'MISSED PACKET', packet, packet_old
-
 	packet_old = packet
 
-	# if ii == 0:
-	# 	FT_hist = FT
-	# 	ACC_hist = ACC
-	# 	if inputs['graphing'] == 2:
-	# 		V_hist = Serial2M40Volts(dat)
-	FT_hist[ii,:] = Serial2FT(dat, bias)
-	ACC_hist[ii,:] = Serial2Acc(dat)
+	DAT_hist[ii, 0:15] = Serial2Data(dat, bias)
 
 	if inputs['graphing'] == 2:
-		V_hist[ii,:] = Serial2M40Volts(dat)
+		DAT_hist[ii,15:] = Serial2M40Volts(dat)
 
-	# Update Graph code
+	# Update Graph
 	if inputs['graphing']:
-		if ii % inputs['update_interval'] == 0 and ii > line_length:
-
-			current_frame = ii + 1
-			if inputs['graphing'] == 1:
-
-				FXline.set_ydata(FT_hist[(current_frame - line_length):current_frame,0].T)
-				FYline.set_ydata(FT_hist[(current_frame - line_length):current_frame,1].T)
-				FZline.set_ydata(FT_hist[(current_frame - line_length):current_frame,2].T)
-				TXline.set_ydata(FT_hist[(current_frame - line_length):current_frame,3].T)
-				TYline.set_ydata(FT_hist[(current_frame - line_length):current_frame,4].T)
-				TZline.set_ydata(FT_hist[(current_frame - line_length):current_frame,5].T)
-
-			if inputs['graphing'] == 2:
-
-				C0line.set_ydata(V_hist[(current_frame- line_length):current_frame,0].T)
-				C1line.set_ydata(V_hist[(current_frame- line_length):current_frame,1].T)
-				C2line.set_ydata(V_hist[(current_frame- line_length):current_frame,2].T)
-				C3line.set_ydata(V_hist[(current_frame- line_length):current_frame,3].T)
-				C4line.set_ydata(V_hist[(current_frame- line_length):current_frame,4].T)
-				C5line.set_ydata(V_hist[(current_frame- line_length):current_frame,5].T)
+		if ii % inputs['update_interval'] == 0 and ii > inputs['line_length']:
+				current_frame = ii + 1
+				updated_data = DAT_hist[(current_frame - inputs['line_length']):current_frame,:]
+				GraphingUpdater(inputs, updated_data, plot_objects)
 			
-			if inputs['graphing'] == 3:
-
-				A1Xline.set_ydata(ACC_hist[(current_frame - line_length):current_frame,0].T)
-				A1Yline.set_ydata(ACC_hist[(current_frame - line_length):current_frame,1].T)
-				A1Zline.set_ydata(ACC_hist[(current_frame - line_length):current_frame,2].T)
-
-				A2Xline.set_ydata(ACC_hist[(current_frame - line_length):current_frame,3].T)
-				A2Yline.set_ydata(ACC_hist[(current_frame - line_length):current_frame,4].T)
-				A2Zline.set_ydata(ACC_hist[(current_frame - line_length):current_frame,5].T)
-
-				A3Xline.set_ydata(ACC_hist[(current_frame - line_length):current_frame,6].T)
-				A3Yline.set_ydata(ACC_hist[(current_frame - line_length):current_frame,7].T)
-				A3Zline.set_ydata(ACC_hist[(current_frame - line_length):current_frame,8].T)
-
-			if inputs['graphing'] == 4:
-
-				if abs(FT[2]) > .1:
-					x = -1*FT[4]/FT[2]
-					y = FT[3]/FT[2]
-				else:
-					x = y = 0
-
-				touch_point.set_ydata(y)
-				touch_point.set_xdata(x)
-
-			pl.draw()
-
+# Send stop byte and get rid of any unread data
 ser.write('\x02')
 ser.flush()
-ser.close()
 print 'Finished Sampling'
 print time.time()-start
-## RECORDING
-# Saves data in timestamped .csv in TestData folder, creates folders if needed
+
+## RECORDING 
+# Saves data in timestamped .csv in TestData\CurrentDate folder,
+# creates the folders if needed
 
 if inputs['write_data'] == 1:
 

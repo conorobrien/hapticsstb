70b4b2ffdd08cfb03da25dd6864f346f951df8fe
Conor O'Brien <conoreobrien@gmail.com>
2014-12-01 12:31:55 -0500

    Clarified install_dep.sh, removed OpenCV download

--- install_dep.sh
+++ install_dep.sh
@@ -1,16 +1,17 @@
 
 sudo apt-get update && apt-get upgrade
 
-sudo apt-get install -y exfat-fuse
-sudo apt-get install -y build-essential vlc v4l-utils p7zip-full yasm
-sudo apt-get install -y python-dev python-numpy python-matplotlib python-opencv python-pip
+sudo apt-get install -y exfat-fuse # fuse for EXFAT file system, used for backup
+sudo apt-get install -y build-essential vlc v4l-utils p7zip-full yasm # general utilities for viewing and compiling
+sudo apt-get install -y python-dev python-numpy python-matplotlib python-opencv python-pip # necessary python modules
 
 sudo pip install --upgrade pip
 sudo pip install pyserial cython
 
-wget http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.9/opencv-2.4.9.zip
-unzip opencv-2.4.9.zip
-rm opencv-2.4.9.zip
+# Optional, download OpenCV
+# wget http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.9/opencv-2.4.9.zip
+# unzip opencv-2.4.9.zip
+# rm opencv-2.4.9.zip
 
 # if this works, also compile ffmpeg from source
 
